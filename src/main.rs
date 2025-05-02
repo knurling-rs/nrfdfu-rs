d8735b59d3e5c3dba6d26c8080a1ab614e664812
chrysn <chrysn@fsfe.org>
2025-05-02 23:49:43 +0200

    Make using a firmware file optional
    This allows probing the connected dongle without sending a file.

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
 #[command(version, about, long_about = None)]
 struct Args {
     /// File to flash
-    elf_path: std::path::PathBuf,
+    elf_path: Option<std::path::PathBuf>,
 }
 
 fn run() -> Result<()> {
@@ -48,9 +48,13 @@ fn run() -> Result<()> {
 
     let args = Args::parse();
 
-    let elf = fs::read(&args.elf_path)
-        .map_err(|e| format!("couldn't read `{}`: {}", args.elf_path.to_string_lossy(), e))?;
-    let mut image = elf::read_elf_image(&elf)?;
+    let image = if let Some(elf_path) = &args.elf_path {
+        let elf = fs::read(elf_path)
+            .map_err(|e| format!("couldn't read `{}`: {}", elf_path.to_string_lossy(), e))?;
+        Some(elf::read_elf_image(&elf)?)
+    } else {
+        None
+    };
 
     let matching_ports: Vec<_> = available_ports()?
         .into_iter()
@@ -101,15 +105,17 @@ fn run() -> Result<()> {
     let hw_version = conn.fetch_hardware_version()?;
     log::debug!("hardware version: {:?}", hw_version);
 
-    // The firmware image must be padded with 0xFF to be a multiple of 4 Bytes. To our knowledge,
-    // this is undocumented.
-    while image.len() % 4 != 0 {
-        image.push(0xff);
-    }
+    if let Some(mut image) = image {
+        // The firmware image must be padded with 0xFF to be a multiple of 4 Bytes. To our knowledge,
+        // this is undocumented.
+        while image.len() % 4 != 0 {
+            image.push(0xff);
+        }
 
-    let init_packet = init_packet::build_init_packet(&image);
-    conn.send_init_packet(&init_packet)?;
-    conn.send_firmware(&image)?;
+        let init_packet = init_packet::build_init_packet(&image);
+        conn.send_init_packet(&init_packet)?;
+        conn.send_firmware(&image)?;
+    }
 
     Ok(())
 }
